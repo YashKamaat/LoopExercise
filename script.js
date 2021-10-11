d80bb12a1a23823418ab6e0d2301f25649c0ca45
Yash Kamaat <yashtkamaat@gmail.com>
2021-10-11 18:30:43 -0400

    Completed exercise

---
+++ script.js
@@ -0,0 +1,74 @@
+// Loops Exercise
+// 1
+for (i = 1; i <= 7; i++){
+    console.log(i);
+  }
+//   OR
+/*for (i = 0; i < 8; i++){
+  console.log(i);
+}
+*/
+// ==========
+// 2
+for(i = 6; i <= 26; i += 4){
+    console.log(i);
+  }
+// ==========
+// 3a
+const wizards = [
+    "Harry Potter",
+    "Hermione Granger",
+    "Ron Weasley"
+  ];
+// ==========
+// 3b
+for (name of wizards){
+    console.log(name);
+  }
+// ==========
+// 4a
+let harryPotterMovies = 0;
+// ==========
+// 4b
+ while (harryPotterMovies <= 8){
+   console.log(harryPotterMovies);
+ harryPotterMovies++;
+   }
+// ==========
+// 4c
+  while (true){
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+    if (harryPotterMovies <= 8){
+      break;
+    }
+  }
+// ==========
+// BONUS
+// ==========
+// 5a
+const hogwartsHouses = [
+    "Gryffindor",
+    "Hufflepuff",
+    "Ravenclaw",
+    "Slytherin"
+  ];
+// ==========
+// 5b
+for (char of hogwartsHouses){
+    for (houses of char){
+      console.log(houses);
+    }
+  };
+//   ========
+// 6a
+const quote = [
+    "Yer",
+    "A",
+    "Wizard",
+    "Harry"
+  ];
+// ==============
+  // 6b
+let oneLine = "";
+for ()
