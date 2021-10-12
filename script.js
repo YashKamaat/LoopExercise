79b638b8eb9d36cad8022db36a807ebd4d72b04b
Yash Kamaat <yashtkamaat@gmail.com>
2021-10-12 10:26:19 -0400

    Edits

--- script.js
+++ script.js
@@ -1,79 +1,78 @@
-// // Loops Exercise
-// // 1
-// for (i = 1; i <= 7; i++){
-//     console.log(i);
-//   };
-// //   OR
-// /*for (i = 0; i < 8; i++){
-//   console.log(i);
-// }
-// */
-// // ==========
-// // 2
-// for(i = 6; i <= 26; i += 4){
-//     console.log(i);
-//   };
-// // ==========
-// // 3a
-// const wizards = [
-//     "Harry Potter",
-//     "Hermione Granger",
-//     "Ron Weasley"
-//   ];
-// // ==========
-// // 3b
-// for (name of wizards){
-//     console.log(name);
-//   };
-// // ==========
-// // 4a
-// let harryPotterMovies = 0;
-// // ==========
-// // 4b
-//  while (harryPotterMovies <= 8){
-//    console.log(harryPotterMovies);
-//  harryPotterMovies++;
-//    };
-// // ==========
-// // 4c
-//   while (true){
-//     console.log(harryPotterMovies);
-//     harryPotterMovies++;
-//     if (harryPotterMovies <= 8){
-//       break;
-//     }
-//   };
-// // ==========
-// // BONUS
-// // ==========
-// // 5a
-// const hogwartsHouses = [
-//     "Gryffindor",
-//     "Hufflepuff",
-//     "Ravenclaw",
-//     "Slytherin"
-//   ];
-// // ==========
-// // 5b
-// for (char of hogwartsHouses){
-//     for (houses of char){
-//       console.log(houses);
-//     }
-//   };
-// //   ========
-// // 6a
-// const quote = [
-//     "Yer",
-//     "A",
-//     "Wizard",
-//     "Harry"
-//   ];
-// ==============
-  // 6b
-// let you = "";
-// while (quote.join){
-//   console.log(quote.join(""));
-//   break;
-// };
+// Loops Exercise
+// 1
+for (i = 1; i <= 7; i++){
+    console.log(i);
+  };
+//   OR
+/*for (i = 0; i < 8; i++){
+  console.log(i);
+}
 
+// ==========
+// 2
+for (i = 5; i <= 25; i += 4){
+    console.log(i);
+  };
+// ==========
+// 3a
+const wizards = [
+    "Harry Potter",
+    "Hermione Granger",
+    "Ron Weasley"
+  ];
+// ==========
+// 3b
+for (name of wizards){
+    console.log(name);
+  };
+// ==========
+// 4a
+let harryPotterMovies = 0;
+// ==========
+// 4b
+//  while (harryPotterMovies <= 7){
+//  harryPotterMovies++;
+ }
+// OR
+while (true){
+  harryPotterMovies++;
+  if (HarryPotterMovies === 8){
+    break;
+  }
+}
+// ==========
+// 4c
+//   console.log(harryPotterMovies);
+// ==========
+// BONUS
+// ==========
+// 5a
+const hogwartsHouses = [
+    "Gryffindor",
+    "Hufflepuff",
+    "Ravenclaw",
+    "Slytherin"
+  ];
+// ==========
+// 5b
+for (char of hogwartsHouses){
+    for (houses of char){
+      console.log(houses);
+    }
+  }
+//   ========
+// 6a
+const quote = [
+    "Yer",
+    "A",
+    "Wizard",
+    "Harry"
+  ];
+==============
+  6b
+let you = "";
+while (quote !== you){
+  console.log(quote.join(""));
+  break;
+};
 
